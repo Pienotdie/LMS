cc9f6af83c4c6e350c704817f654383c13d93c0d
Pienotdie <wwwwoooolk@gmail.com>
2024-05-17 23:06:47 +0300

    connecting an authentication tool Clerk

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,13 @@
+const AuthLa = ({
+     children
+}: {
+     children: React.ReactNode
+}) => {
+     return ( 
+          <div className="h-full flex items-center justify-center">
+               {children}
+          </div>
+      );
+}
+ 
+export default AuthLa;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
-
-   <p> This is a protected page</p>
-
+     <UserButton
+          afterSignOutUrl='/'
+     />
   )
 }
