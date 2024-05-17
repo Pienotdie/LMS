b2a345a885149e94deee1af5f9c15322b7d6e61d
Pienotdie <wwwwoooolk@gmail.com>
2024-05-17 15:02:19 +0200

    creating an app

---
+++ lms/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+    <p className="text-3xl font-medium text-sky-700">
+      Hello
+    </p>
+    <Button variant="destructive">
+      Click me
+    </Button>
+    </div>
+  )
+}
